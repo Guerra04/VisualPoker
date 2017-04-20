eaa84ee88f6336dde1c0c4765fbd43dd9c9a7897
Guerra04 <danieljdguerra@hotmail.com>
2017-04-20 16:56:58 +0100

    Comparator func

---
+++ Project/src/cards/ComparatorByRank.java
@@ -0,0 +1,14 @@
+package cards;
+
+import java.util.Comparator;
+
+public class ComparatorByRank implements Comparator<Card> {
+	public int compare(Card c1, Card c2){
+		if(c1.rank > c2.rank)
+			return 1;
+		else if(c1.rank == c2.rank)
+			return 0;
+		else
+			return -1;
+	}
+}
