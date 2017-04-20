930087e9726379f544e3477970484e6ed9c715d8
ElTenente <sergiomgn@hotmail.com>
2017-04-20 16:58:00 +0100

    Sou o Mário
    Verificar algumas scores da mão

--- Project/src/player/Hand.java
+++ Project/src/player/Hand.java
@@ -69,39 +69,49 @@ public class Hand {
 		return samesuit;
 	}
 	
-	
- 	private String BestMove(Hand hand){
-		String advise = null;
+	private int handScore(Hand hand){
+		int score;	
+		Card[] sortedhand = hand.hand;
 		
-		/*Verifying if it's a Straight Flush*/
+		java.util.Arrays.sort(sortedhand, new ComparetorByRank);
+		
 		int[] suit;
-		int ranking = 1;
-		int discard = 0;
+		
+		/*Checking if the hand is a straight*/
+		int straight=1;
+		
+		for (int i=0; i<4; i++){
+			if(sortedhand[i].rank == sortedhand[i+1].rank - 1)
+				straight++;
+			if(straight==4){
+				if(sortedhand[0].rank == 1 && sortedhand[1].rank==10)
+					straight++;
+			}
+		}
+		/*Checking if hand is flush*/
+		int flush=0;
 		
 		suit = isSameSuit(hand);
+		if (suit[0]==5)
+			flush=1;
 		
-		if(suit[0]==5){								
-			for(int i=0; i<5;i++){
+		/*Is straight flush? */
 	
-				if(hand.hand[i].rank == 1){
-					ranking++;	
-					continue;
-				}
-				
-				if(hand.hand[i].rank % 10 == i){
-					ranking ++;
-				}
-				else
-					discard = i;
-				
-			}
-			if(ranking==5)											//Royal Flush
-				advise = "Keep Straight Flush!";
-			
-			if(ranking==4)											//Almost royal Flush
-				advise = "Discard" + hand.hand[discard].toString();
+		if(straight==5){
+			if(flush==1)
+				if(sortedhand[1].rank==10)	//Nigga cagou-se todo!!!!!
+					score=1;				
+				else			//Nigga has a straight flush
+					score=5;
+			else				//Nigga has a straight
+				score=8;
 		}
-		
+		/**/
+		return score;
+	}
+	
+ 	private String BestMove(Hand hand){
+		String advise = null;	
 		return advise;
 	}
 }
